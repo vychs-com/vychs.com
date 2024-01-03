817d69e63fc4b4ec0c169f30efa19eafa47b5955
Vyacheslav <slava021323@gmail.com>
2024-01-04 01:05:41 +0600

    feat: minor

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,13 +1,38 @@
 import { faGithub, faTelegram } from '@fortawesome/free-brands-svg-icons'
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 import { Container } from '@suid/material'
 import Fa from 'solid-fa'
-import type { Component } from 'solid-js'
+import { Component, For, Show, createSignal, onMount } from 'solid-js'
 import logo from '../assets/logo.svg'
+import { Project } from '../types/Project'
 
 export const MainPage: Component = () => {
-    const openProjectPage = (subdomain: string) => {
-        window.open('https://' + subdomain + '.vychs.com', '_self')
+    const [projects, setProjects] = createSignal<Project[]>([])
+
+    const openPage = (url: string | undefined) => {
+        if (!url) return
+        window.open(url, '_self')
     }
+
+    onMount(() => {
+        setProjects([
+            {
+                title: 'Random Animals',
+                description:
+                    'Create dynamic random animal pictures using Telegram icons from various platforms. Explore customization options to tailor the generated images to your preferences and easily download the pictures for your enjoyment. Web version.',
+                web_url: 'https://animals.vychs.com',
+                tg_url: 'https://t.me/randanimalbot',
+                source_url: 'https://github.com/vychs-com/animals.vychs.com',
+            },
+            {
+                title: 'SolidJs Chart.js',
+                description:
+                    'Developed with SolidJs, this package offers seamlessly integrated components for Chart.js. Explore the demo webpage showcasing multiple components from the package, accompanied by manipulation buttons that allow you to see the dynamic nature of the charts in real-time.',
+                web_url: 'https://solid-chartjs.vychs.com/',
+                source_url: 'https://github.com/s0ftik3/solid-chartjs',
+            },
+        ])
+    })
 
     return (
         <Container maxWidth="sm">
@@ -28,6 +53,13 @@ export const MainPage: Component = () => {
                         <Fa icon={faTelegram} />
                     </a>
                     <a
+                        href="https://t.me/vhslaugh"
+                        target="_blank"
+                        style={{ 'margin-left': '10px' }}
+                    >
+                        <Fa icon={faTelegram} />
+                    </a>
+                    <a
                         href="https://github.com/s0ftik3"
                         target="_blank"
                         style={{ 'margin-left': '10px' }}
@@ -37,26 +69,47 @@ export const MainPage: Component = () => {
                 </div>
             </div>
             <div class="content">
-                <div class="card" onClick={() => openProjectPage('animals')}>
-                    <div class="card__title">Random Animals</div>
-                    <div class="card__description">
-                        Create a dynamic random animal pictures using Telegram
-                        icons from various platforms. Explore customization
-                        options to tailor the generated images to your
-                        preferences and easily download the pictures for your
-                        enjoyment.
-                    </div>
-                </div>
-
-                <div
-                    class="card"
-                    onClick={() => openProjectPage('solid-chartjs')}
-                >
-                    <div class="card__title">SolidJs Chart.js</div>
-                    <div class="card__description">
-                        SolidJs components for Chart.js. Demo webpage.
-                    </div>
-                </div>
+                <For each={projects()}>
+                    {(project: Project) => (
+                        <div class="card">
+                            <div class="card__title">{project.title}</div>
+                            <Show when={project.description}>
+                                <div class="card__description">
+                                    {project.description}
+                                </div>
+                            </Show>
+                            <div class="card__footer">
+                                <Show when={project.source_url}>
+                                    <a
+                                        href={project.source_url}
+                                        target="_blank"
+                                    >
+                                        <Fa icon={faGithub} /> Source Code
+                                    </a>
+                                </Show>
+                                <Show when={project.tg_url}>
+                                    <button
+                                        class="open-tg-bot-btn"
+                                        onClick={() => openPage(project.tg_url)}
+                                    >
+                                        <Fa icon={faTelegram} />
+                                    </button>
+                                </Show>
+                                <Show when={project.web_url}>
+                                    <button
+                                        class="open-project-btn"
+                                        onClick={() =>
+                                            openPage(project.web_url)
+                                        }
+                                    >
+                                        <Fa icon={faArrowUpRightFromSquare} />{' '}
+                                        Open
+                                    </button>
+                                </Show>
+                            </div>
+                        </div>
+                    )}
+                </For>
             </div>
         </Container>
     )
---
+++ src/types/Project.ts
@@ -0,0 +1,7 @@
+export interface Project {
+    title: string
+    description?: string
+    web_url?: string
+    tg_url?: string
+    source_url?: string
+}
